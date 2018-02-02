374efe96022e81f206513397d5de9c0d5a2a3676
Adam Rizzo <adamjrizzo@yahoo.com>
2018-02-02 14:09:24 -0500

    Initial Commit

---
+++ app.js
@@ -0,0 +1,29 @@
+// global variables
+
+
+
+$(document).ready(function() {
+  var select = document.getElementById("selectTeam");
+  var teamList = ["Atlanta Hawks", "Boston Celtics", "Brooklyn Nets", "Charlotte Hornets"];
+
+  for(var i = 0; i < teamList.length; i++) {
+    var opt = teamList[i];
+    var el = document.createElement("option");
+    el.textContent = opt;
+    el.value = opt;
+    select.appendChild(el);
+  }​
+});
+
+function logInToDashboard() {
+  var userName = document.getElementById('uname').value;
+  var password = document.getElementById('pwd').value;
+
+  console.log("userName = " + userName);
+  console.log("password = " + password);
+
+  if (userName == "R1zzo23" && password == "jordan"){
+    //window.location.href = "dashboard.html";
+    window.open("dashboard.html");
+  }
+}
