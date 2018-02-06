9c375c06b991bc6b57d5a813fc60ff16531145b8
Adam Rizzo <adamjrizzo@yahoo.com>
2018-02-06 13:38:53 -0500

    Got rid of team select dropdown box and replaced with username text entry.

--- app.js
+++ app.js
@@ -3,16 +3,7 @@
 
 
 $(document).ready(function() {
-  /*var select = document.getElementById("selectTeam");
-  var teamList = ["Atlanta Hawks", "Boston Celtics", "Brooklyn Nets", "Charlotte Hornets"];
-
-  for(var i = 0; i < teamList.length; i++) {
-    var opt = teamList[i];
-    var el = document.createElement("option");
-    el.textContent = opt;
-    el.value = opt;
-    select.appendChild(el);
-  }​*/
+  
 });
 
 function logInToDashboard() {
@@ -22,4 +13,11 @@ function logInToDashboard() {
   if (userName == "R1zzo23" && password == "jordan"){
     window.open("https://r1zzo23.github.io/CSL_Scouting_Site/dashboard.html");
   }
+  else {
+    alert("Username and password do not match our records. Please try again.");
+  }
+}
+
+function createUser(userName) {
+
 }
